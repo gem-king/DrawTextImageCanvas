77e639661281c30e8296529fdff3f038181021a3
hoangdshp123 <fantasticalhp99@gmail.com>
2017-12-08 18:09:05 +0700

    update

---
+++ app/src/main/java/com/example/admin/drawtextimagecanvas/MainActivity.java
@@ -0,0 +1,247 @@
+package com.example.admin.drawtextimagecanvas;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLoad, drawBitmap, btnsave,btnhienthi;
+    private TextView text;
+    private ImageView img;
+    ProgressDialog progressDialog;
+    private final int RQS_IMAGE = 1;
+    Uri uri;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    public static final int REQUEST_TAKE_PHOTO = 0;
+    public static final int MEDIA_TYPE_IMAGE = 4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        super.onCreate(savedInstanceState);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Lưu vào DCIM");
+        progressDialog.setMessage("Loading...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+
+        setContentView(R.layout.activity_main);
+        init();
+
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                if (uri == null) {
+//                    Toast.makeText(this, "There was a problem accessing your device's external storage.",Toast.LENGTH_LONG).show();
+                } else {
+                    Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                    startActivityForResult(takePhotoIntent, REQUEST_TAKE_PHOTO);
+                }
+            }
+        });
+
+        drawBitmap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RQS_IMAGE);
+            }
+        });
+
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bitmap bitmapDraw = bitmapDraw();
+                if (bitmapDraw != null)
+                storeImage(bitmapDraw);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == RQS_IMAGE) {
+                if (data != null) {
+                    uri = data.getData();
+                    text.setText(uri.toString());
+                }
+            }
+        }
+    }
+
+    private Bitmap bitmapDraw() {
+        Bitmap bitmap = null;
+        Bitmap newBitmap = null;
+
+        try {
+            bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+            Bitmap.Config config = bitmap.getConfig();
+            if (config == null) {
+                config = Bitmap.Config.ARGB_8888;
+            }
+
+            newBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), config);
+            Canvas canvas = new Canvas(newBitmap);
+            canvas.drawBitmap(bitmap, 0, 0, null);
+            DateFormat dateFormatter1 = new SimpleDateFormat("dd-MM-yyyy");
+            DateFormat dateFormatter2 = new SimpleDateFormat("hh:mm:ss");
+            dateFormatter1.setLenient(false);
+            dateFormatter2.setLenient(false);
+            java.util.Date today = new java.util.Date();
+            String caption = dateFormatter1.format(today);
+            String time = dateFormatter2.format(today);
+
+            if (caption != null) {
+                Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+                paint.setColor(Color.RED);
+                paint.setTextSize(50);
+                paint.setStyle(Paint.Style.FILL);
+                paint.setShadowLayer(10f, 10f, 10f, Color.BLACK);
+
+                Rect rect = new Rect();
+                paint.getTextBounds("" + caption + ", " + time, 0, caption.length(), rect);
+
+                canvas.drawText("" + caption + ", " + time, 0, rect.height(), paint);
+                canvas.save();
+
+                Toast.makeText(this, "drawText: " + caption, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Caption empty", Toast.LENGTH_SHORT).show();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return newBitmap;
+    }
+
+
+    private Uri getOutputMediaFileUri(int mediaType) {
+        // check for external storage
+        if (isExternalStorageAvailable()) {
+            // get the URI
+
+            // 1. Get the external storage directory
+            File mediaStorageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+            // 2. Create a unique file name
+            String fileName = "";
+            String fileType = "";
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+            if (mediaType == MEDIA_TYPE_IMAGE) {
+                fileName = "IMG_" + timeStamp;
+                fileType = ".jpg";
+            }
+                else {
+                return null;
+            }
+            // 3. Create the file
+            File mediaFile;
+            try {
+                mediaFile = File.createTempFile(fileName, fileType, mediaStorageDir);
+                Log.i(TAG, "File: " + Uri.fromFile(mediaFile));
+                // 4. Return the file's URI
+                return Uri.fromFile(mediaFile);
+            }
+            catch (IOException e) {
+                Log.e(TAG, "Error creating file: " +
+                        mediaStorageDir.getAbsolutePath() + fileName + fileType);
+            }
+        }
+
+        // something went wrong
+        return null;
+    }
+
+    private boolean isExternalStorageAvailable(){
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    private  File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+//        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+//                + "/Android/data/"
+//                + getApplicationContext().getPackageName()
+//                + "/Files");
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                + "/DCIM/");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
+        File mediaFile;
+        String mImageName="MI_"+ timeStamp +".jpg";
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+        return mediaFile;
+    }
+    private void storeImage(Bitmap image) {
+        File pictureFile = getOutputMediaFile();
+        if (pictureFile == null) {
+            Log.d(TAG,
+                    "Error creating media file, check storage permissions: ");// e.getMessage());
+            return;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            image.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.close();
+            Toast.makeText(this, "Save thành công", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+        }
+    }
+    private void init() {
+        btnLoad = (Button) findViewById(R.id.button);
+        drawBitmap = (Button) findViewById(R.id.button2);
+        btnsave = findViewById(R.id.btn_save);
+        text = (TextView) findViewById(R.id.text);
+        img = (ImageView) findViewById(R.id.image);
+    }
+}
